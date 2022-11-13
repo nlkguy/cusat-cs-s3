a4342f4212a1f61c80047474410e1218cad1f2b8
nlkguy <nandulal299792.458@gmail.com>
2022-11-13 19:51:34 +0530

    minor changes

--- 308-oop-lab/refresherCPP/fib.cpp
+++ 308-oop-lab/refresherCPP/fib.cpp
@@ -6,10 +6,18 @@ using namespace std;
 
 // 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55....
 
+
+int fibRecursion(int num){
+	if(num<=1){
+		return num;
+	}
+	return fibRecursion(num-1) + fibRecursion(num-2);
+}
+
 int main(){
 
 	int n1 = 0 , n2 = 1,n3;
-	int limit = 10;
+	int limit = 10; // can also get value from user
 	cout<<n1<<", "<<n2;
 	for(int i=2;i<=limit;i++){
 		n3 = n1+n2;
@@ -18,6 +26,16 @@ int main(){
 		n2 = n3;
 		
 	}
-	cout<<endl;
+	cout<<"\nUsing Recursion : "<<fibRecursion(limit)<<endl;
 	
 }
+
+/*
+
+// other methods are also present
+// recurrence relation : F(n) = F(n-1) + F(n-2)
+// pseudo code : 
+
+*/
+
+// refer : https://www.geeksforgeeks.org/program-for-nth-fibonacci-number/
